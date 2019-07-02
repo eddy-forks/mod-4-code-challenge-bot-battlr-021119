18ea2922844a8ba9926ce81bc8be3a1ba57f7891
github username <github email address>
2019-07-02 13:05:32 -0400

    done with checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,14 +2,26 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
+  // constructor(props){
+  //   super(props)
+  //   this.state = {bots: this.props.bots}
+  // }
+
   //your code here
+  botsRender = () => {
+
+    return this.props.bots.map(bot => (
+      <BotCard key={bot.id} bot={bot} handleClick={this.props.handleClick} />))
+  }
+
+
 
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
     		  Collection of all bots
+          {this.botsRender()}
     		</div>
   	  </div>
   	);
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -3,20 +3,26 @@ import BotCard from "../components/BotCard";
 
 class YourBotArmy extends React.Component {
   //your bot army code here...
+  handleClick = (e) => {e.preventDefault()}
+  botsRender = (yourBots) => {
+    console.log(yourBots)
+    return yourBots.map(bot => (<BotCard bot={bot} handleClick={this.handleClick}/>))
+  }
+
 
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
             Your Bot Army
+            {this.botsRender(this.props.yourBots)}
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
